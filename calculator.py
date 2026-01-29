e49a7712be8495252838fb44e44931a821d87456
ishan <g00562215@gmail.com>
2026-01-29 17:27:18 +0530

    full calculator

--- calculator.py
+++ calculator.py
@@ -4,15 +4,26 @@ def add(a,b):
 def sub(a,b):
     return a - b
 
+def multiply(a,b):
+    return a * b
+
+def divide(a,b):
+    if b == 0:
+        return "Cannot divide by zero"
+    
+    return a / b
+
 def main():
     while True:
         print("\nSimlpe Calculator")
         print("1.Add")
         print("2.Subtract")
-        print("3.Exit")
+        print("3.Multiply")
+        print("4.Divide")
+        print("5.Exit")
 
         choice = int(input("Enter your choice: "))
-        if choice == 3:
+        if choice == 5:
             print("Goodbuy")
             break
 
@@ -24,6 +35,12 @@ def main():
         
         elif choice == 2:
             print("Result is ",sub(num1, num2))
+        
+        elif choice == 3:
+            print("Result is ",multiply(num1, num2))
+
+        elif choice == 4:
+            print("Result is ",divide(num1, num2))
         
         else:
             print("Envalid number")
